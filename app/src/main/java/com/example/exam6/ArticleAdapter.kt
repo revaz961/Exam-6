0ec86117213dfead98819f0bb8cdca52e6db463d
revaz <revaz961@gmail.com>
2021-06-11 10:52:00 +0400

    exam 6

--- app/src/main/java/com/example/exam6/ArticleAdapter.kt
+++ app/src/main/java/com/example/exam6/ArticleAdapter.kt
@@ -3,6 +3,7 @@ package com.example.exam6
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.exam6.databinding.ArticleLayoutBinding
 
 class ArticleAdapter(val update:(Article)->Unit,val delete: (Article) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -36,6 +37,9 @@ class ArticleAdapter(val update:(Article)->Unit,val delete: (Article) -> Unit) :
         fun bind() {
             binding.tvTitle.text = articles[adapterPosition].title
             binding.tvDescription.text = articles[adapterPosition].description
+            Glide.with(binding.root.context)
+                .load(articles[adapterPosition].url)
+                .into(binding.ivImage);
 
             binding.btnRemove.setOnClickListener {
                 delete(articles[adapterPosition])
